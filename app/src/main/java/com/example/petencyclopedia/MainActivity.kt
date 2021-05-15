34ca2c7ddb10bee3a518e93ba36a08bf881ccb47
Arkani66 <prospa@et.esiea.fr>
2021-05-15 16:59:57 +0200

    add naviguation_choice activity

--- app/src/main/java/com/example/petencyclopedia/MainActivity.kt
+++ app/src/main/java/com/example/petencyclopedia/MainActivity.kt
@@ -2,11 +2,28 @@ package com.example.petencyclopedia
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var m_textviewaccueil : TextView
+    private lateinit var m_buttonaccueil : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        m_textviewaccueil = findViewById(R.id.accueil_text)
+        m_buttonaccueil = findViewById(R.id.accueil_button)
+
+        m_buttonaccueil.setOnClickListener (View.OnClickListener{
+            Toast.makeText(this@MainActivity, R.string.message_accueil_btn, Toast.LENGTH_LONG)
+            .show()
+            //startActivity()
+        } )
     }
+
 }
---
+++ app/src/main/java/com/example/petencyclopedia/Naviguation_Choice.kt
@@ -0,0 +1,35 @@
+package com.example.petencyclopedia
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.petencyclopedia.databinding.ActivityNaviguationChoiceBinding
+
+class Naviguation_Choice : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNaviguationChoiceBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNaviguationChoiceBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_naviguation_choice)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
